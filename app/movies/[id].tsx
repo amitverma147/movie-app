fb95f1e653e4a4b435d0b6e387a5c85d4bccdb3b
Amit Verma <vamit20000@gmail.com>
2025-09-05 01:55:35 +0530

    app developement- routing and navigation

---
+++ app/movies/[id].tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const MovieDetail = () => {
+  return (
+    <View>
+      <Text>[id]</Text>
+    </View>
+  );
+};
+
+export default MovieDetail;
+
+const styles = StyleSheet.create({});

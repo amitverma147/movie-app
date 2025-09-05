5ece7923ca8abe7bcc45b1aea33178faa6124fe5
Amit Verma <vamit20000@gmail.com>
2025-09-06 01:23:19 +0530

    tab bar done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,11 @@
 import { Stack } from "expo-router";
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen
-    name="(tabs)"
-    options={{headerShown:false}}
-    />
-    <Stack.Screen 
-    name="movies/[id]"
-    options={{headerShown:false}}
-    />
-  </Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="movies/[id]" options={{ headerShown: false }} />
+    </Stack>
+  );
 }
